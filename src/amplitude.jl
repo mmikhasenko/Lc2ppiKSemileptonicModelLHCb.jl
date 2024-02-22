04984e670c0455a5b8b0c366cff9279ca278e064
Mikhail Mikhasenko <mikhail.mikhasenko@cern.ch>
2024-02-22 14:43:03 +0100

    replaced Lc2ppiKModel to ThreeBodyDecay

--- src/amplitude.jl
+++ src/amplitude.jl
@@ -1,35 +1,11 @@
-struct Lc2ppiKModel{N, D <: DecayChain, L <: Number}
-	chains::SVector{N, D}
-	couplings::SVector{N, L}
-	isobarnames::SVector{N, String}
-end
-
 function Lc2ppiKModel(; chains, couplings, isobarnames)
-	N = length(chains)
-	N != length(couplings) && error("Length of couplings does not match the length of the chains")
-	N != length(isobarnames) && error("Length of isobarnames does not match the length of the chains")
-	#
-	v = collect(zip(chains, couplings, isobarnames))
-	sort!(v, by = x -> eval(Meta.parse(x[3][3:end-1])))
-	sort!(v, by = x -> findfirst(x[3][1], "LDK"))
-	#
-	sort_chains, sort_couplings, sort_isobarnames =
-		getindex.(v, 1), getindex.(v, 2), getindex.(v, 3)
-	#
-	Ttbs = typeof(chains[1].tbs)
-	sv_sort_chains = (SVector{N, <:DecayChain})(sort_chains)
-	sv_sort_couplings = SVector{N}(sort_couplings)
-	sv_sort_isobarnames = SVector{N}(sort_isobarnames)
-	#
-	Lc2ppiKModel(sv_sort_chains, sv_sort_couplings, sv_sort_isobarnames)
+
+	v = collect(enumerate(isobarnames))
+	sort!(v, by = x -> eval(Meta.parse(x[2][3:end-1])))
+	sort!(v, by = x -> findfirst(x[2][1], "LDK"))
+	order = getindex.(v, 1)
+
+	mypairs = isobarnames[order] .=> zip(couplings[order], chains[order])
+	x = Vector{Pair{String, Tuple{Complex, AbstractDecayChain}}}(mypairs)
+	ThreeBodyDecay(x)
 end
-
-
-amplitude(model::Lc2ppiKModel, σs, two_λs) =
-	sum(c * amplitude(d, σs, two_λs) for (c, d) in zip(model.couplings, model.chains))
-
-unpolarizedintensity(model::Lc2ppiKModel, σs) =
-	sum(abs2, amplitude(model, σs, two_λs)
-			  for two_λs in itr(tbs.two_js))
-# 
-masses(model::Lc2ppiKModel) = first(model.chains).tbs.ms
